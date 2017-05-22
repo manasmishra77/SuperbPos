72b866feff6ddd9eafbd065adf2eca0d432d53c1
Manas Mishra <manasmishra@Prannoys-MacBook-Air.local>
2017-05-23 02:25:27 +0530

    Initial Commit

---
+++ InVisionSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InVisionSwift
+//
+//  Created by Manas Mishra on 23/05/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
