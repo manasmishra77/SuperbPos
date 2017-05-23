0fc05693cde9a9b9a18a3c8a37911d31c047172d
Manas Mishra <manasmishra@Prannoys-MacBook-Air.local>
2017-05-23 14:26:44 +0530

    login screen without apis

---
+++ InVisionSwift/SignInViewController.swift
@@ -0,0 +1,93 @@
+//
+//  SignInViewController.swift
+//  InVisionSwift
+//
+//  Created by Manas Mishra on 23/05/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordImageView: UIImageView!
+    @IBOutlet weak var userNameImageView: UIImageView!
+    @IBOutlet weak var userName: UITextField!
+    
+    var validUserName = false
+    var validPassword = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        password.delegate = self
+        userName.delegate = self
+        passwordImageView.adoptCircularShape()
+        userNameImageView.adoptCircularShape()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        view.endEditing(true)
+        if !(textField.text?.isEmpty){
+            if textField.tag == 101{
+                if password.text?.characters.count > 4{
+                    validPassword = true
+                }else{
+                    let vc = Utilities.alertViewController(title: "Enter valid password", msg: "")
+                    present(vc, animated: true, completion: nil)
+                }
+            }else{
+                validUserName = true
+            }
+        }else{
+            Utilities.alertBubble("Text input can not be empty", view: view)
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldDidEndEditing(textField)
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        validUserName = false
+        validPassword = false
+
+        return true
+    }
+    
+
+    @IBAction func loginButton(_ sender: UIButton) {
+        if validUserName && validPassword{
+            
+        }else if validUserName{
+            let vc = Utilities.alertViewController(title: "Enter valid password", msg: "")
+            present(vc, animated: true, completion: nil)
+        }else{
+            let vc = Utilities.alertViewController(title: "Enter valid username", msg: "")
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    @IBAction func contactUs(_ sender: UIButton) {
+    }
+    @IBAction func onHelpButton(_ sender: Any) {
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'InVisionSwift' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for InVisionSwift
+ pod 'MBProgressHUD', '~> 1.0.0'
+ pod 'SDWebImage'
+
+
+  target 'InVisionSwiftTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'InVisionSwiftUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
